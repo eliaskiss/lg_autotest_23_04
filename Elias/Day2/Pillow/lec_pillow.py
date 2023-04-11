1a84948a376dfd919d8e0e808f2b4197720d650e
elias <eliaskiss@hotmail.com>
2023-04-11 15:13:27 +0900

    Day2

---
+++ Elias/Day2/Pillow/lec_pillow.py
@@ -0,0 +1,8 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from PIL import ImageFilter
+import os
+from enum import Enum
+from icecream import ic
+from datetime import datetime
