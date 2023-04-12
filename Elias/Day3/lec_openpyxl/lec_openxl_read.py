80dc6b7b9f8ea8f26fd715b2da443f95bb59a66a
elias <eliaskiss@hotmail.com>
2023-04-12 09:44:06 +0900

    Day2

--- Elias/Day3/lec_openpyxl/lec_openxl_read.py
+++ Elias/Day3/lec_openpyxl/lec_openxl_read.py
@@ -14,7 +14,7 @@ wb = load_workbook(file_name,
 
 # Get Sheet list
 ws_list = wb.sheetnames
-ic(ws_list)
+# ic(ws_list)
 
 # Select Worksheet
 # ws = wb.active # 현재 활성화되어 있는 sheet(저장할때 선택된 시트)
@@ -24,24 +24,58 @@ ws = wb['항공요금']
 
 # Get Cell Value
 cell_A1 = ws['A1']
-ic(cell_A1)
-ic(cell_A1.value)
+# ic(cell_A1)
+# ic(cell_A1.value)
 
 # Get Formular Cell Value
 cell_D2 = ws['D2']
-ic(cell_D2.value) # data_only의 값에 따라 다른 Cell값을 가져옴, data_only : True --> 8.43, data_only : False --> =A2+B2
+# ic(cell_D2.value) # data_only의 값에 따라 다른 Cell값을 가져옴, data_only : True --> 8.43, data_only : False --> =A2+B2
 
 # Get Datetime Cell Value
 cell_F2 = ws['F2']
-ic(cell_F2.value)
+# ic(cell_F2.value)
 cell_F2_value = cell_F2.value.strftime('%Y-%m-%d %H:%M:%S')
-ic(cell_F2_value)
+# ic(cell_F2_value)
 
+# # Get All 1st Column
+# index = 1
+# while True:
+#     cell = f'A{index}' # A1, A2, A3, ....
+#     cell_value = ws[cell].value
+#
+#     if cell_value == '' or cell_value is None:
+#         break
+#
+#     ic(cell, cell_value)
+#     index += 1
 
+# # Get All 2nd Row
+# index = 0
+# while True:
+#     cell = f'{chr(ord("A") + index)}2' # ord("A") + 1: 42 + 1 = 43, chr(43) --> B : A2, B2, C2, ...
+#     cell_value = ws[cell].value
+#
+#     if cell_value == '' or cell_value is None:
+#         break
+#
+#     ic(cell, cell_value)
+#     index += 1
 
+# # Get row data
+# index = 0
+# for row in ws.rows:
+#     if index != 0:
+#         ic(index, f'Weigh:{row[0].value}', f'Fee:{row[1].value}')
+#     index += 1
 
+# Get row data 2nd way
+index = 1
+for row in ws.iter_rows(min_row=2):
+    ic(index, f'Weigh:{row[0].value}', f'Fee:{row[1].value}')
+    index += 1
 
-
+# read_only=False 인 경우
+wb.close()
 
 
 
